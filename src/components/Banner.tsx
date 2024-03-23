5f49a4f6057b4e1783c603742aa01f1105d5b951
Nitchakarn Jirathunyawong <6633077621@student.chula.ac.th>
2024-03-24 04:17:54 +0700

    2

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -4,8 +4,7 @@ import { useSession } from 'next-auth/react'
 
 
 export default function Banner(){
-   const {data: session} = useSession()
-   console.log(session?.user.token)
+   
    return (
       <div className='block p-[5px] m-0 w-[100vw] h-[90vh] relative'>
          <Image src = {'/img/cover1.png'}
@@ -13,11 +12,7 @@ export default function Banner(){
          fill={true}
          className='object-cover rounded-t-lg'
          priority/>
-            {
-               session? <div className='z-30 absolute top-5 right-10 font-semibold text-white text-xl'>Welcome {session.user?.name}</div>
-                     :null
-            }
-
+           
          
       </div>
       
--- src/components/TopMenu.tsx
+++ src/components/TopMenu.tsx
@@ -1,12 +1,11 @@
 import Image from 'next/image'
 import TopMenuItem from './TopMenuItem'
 import { getServerSession } from 'next-auth'
-import { authOptions } from '@/app/api/auth/[...nextauth]/route'
-import { Link } from '@mui/material'
+
 import UserMenu from './UserMenu'
 
 export default async function TopMenu(){
-   const session = await getServerSession(authOptions)
+ 
 
    return (
       <div className='h-[146px] w-auto bg-white fixed top-0 left-0 right-0 z-30 border-b border-solid border-lightgray-200 border-t-1 border-solid border-lightgray-100 flex flex-row justify-between'>
@@ -17,11 +16,7 @@ export default async function TopMenu(){
             <TopMenuItem title='Booking' pageRef='/booking'/>
             <TopMenuItem title='About Us' pageRef='/about'/>
          </div>
-         {
-            session?<div><Image src={'/img/userlogo.png'} className='h-[40%] mt-11 w-auto mr-3 mb-auto' 
-            alt='logo' width={0} height={0} sizes='100vh'/></div>
-            :<TopMenuItem title='Sign in' pageRef='/api/auth/signin'/>
-         }
+        
       </div>
    )
 }
