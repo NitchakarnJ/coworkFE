fc90559258ce0eca28b5689e40cf9a693aac56ed
Nitchakarn Jirathunyawong <6633077621@student.chula.ac.th>
2024-03-24 21:46:16 +0700

    0

--- src/libs/getCoworking.tsx
+++ src/libs/getCoworking.tsx
@@ -3,7 +3,7 @@ export default async function getCoworking(id:string) {
 
    // await new Promise( (resolve)=> setTimeout(resolve,1000))
 
-   const response = await fetch(`${process.env.BACKEND_URL}/api/project/coworkings/${id}`)
+   const response = await fetch(`https://coworking-reservation-app-isamare.vercel.app/api/project/coworkings/${id}`)
    if(!response.ok){
       throw new Error("Failed to fetch coworking")
    }
--- src/libs/getUserProfile.tsx
+++ src/libs/getUserProfile.tsx
@@ -1,6 +1,6 @@
 export default async function getUserProfile(token:string){
    
-   const response = await fetch(`${process.env.BACKEND_URL}/api/project/auth/me`,{
+   const response = await fetch("https://coworking-reservation-app-isamare.vercel.app/api/project/auth/me",{
       method: "GET",
       headers: {
          authorization: `Bearer ${token}`,
