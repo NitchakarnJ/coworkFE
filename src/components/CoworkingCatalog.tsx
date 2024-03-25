2140c2758f71ed19a03d9b6ff6c42d29173d11d8
Nitchakarn Jirathunyawong <6633077621@student.chula.ac.th>
2024-03-25 13:30:34 +0700

    .

--- src/components/CoworkingCatalog.tsx
+++ src/components/CoworkingCatalog.tsx
@@ -12,8 +12,8 @@ export default async function CoworkingCatalog({coworkingsJson}:{coworkingsJson:
       Explore {coworkingsJsonReady.count} model in our Coworking
       <div className="flex flex-row flex-wrap m-5  justify-around content-around">
             {
-               coworkingsJsonReady.data.map((CoworkingItem:CoworkingItem)=>(
-                  <Link href={`/coworking/${CoworkingItem.id}`} className="w-1/5">
+               coworkingsJsonReady.data.map((CoworkingItem:CoworkingItem,index:number)=>(
+                  <Link href={`/coworking/${CoworkingItem.id}`} className="w-1/5" key={index}>
                      <Card hospitalName={CoworkingItem.name} imgSrc={CoworkingItem.picture} />
                   </Link>
                ))
